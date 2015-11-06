042bdef3253c3bfceedb1488297bb01be8702910
Pygocentrus <pierre.guilhou14@gmail.com>
2015-11-06 12:08:54 +0100

    [architecture] Refactor DRY components for listing

--- app/scripts/components/partials/live/TweetsRowComponent.js
+++ app/scripts/components/partials/live/TweetsRowComponent.js
@@ -6,20 +6,40 @@ import { Link } from 'react-router';
 // Modules
 import Utils from '../../../mixins/utils';
 import LivestreamActions from '../../../actions/LivestreamActions';
+import BnfQueueActions from '../../../actions/BnfQueueActions';
 
 class TweetsRowComponent extends Component {
 
   constructor(props) {
     super(props);
 
+    // Utils
     this._linkToUser = this._linkToUser.bind(this);
     this._linkToOriginalPost = this._linkToOriginalPost.bind(this);
     this._cleanLocation = this._cleanLocation.bind(this);
+
+    // Action handlers
     this._handleValidate = this._handleValidate.bind(this);
     this._handleReject = this._handleReject.bind(this);
+    this._handleCancel = this._handleCancel.bind(this);
+
+    // Dynamic UI components
+    this._actionsLive = this._actionsLive.bind(this);
+    this._actionsQueue = this._actionsQueue.bind(this);
   }
 
   render() {
+
+    let btn;
+
+    switch (this.props.actionsType) {
+      case 'queue':
+        btn = this._actionsQueue();
+        break;
+      case 'live':
+      default:
+        btn = this._actionsLive();
+    }
 
     return (
       <tr>
@@ -29,19 +49,7 @@ class TweetsRowComponent extends Component {
         <td>{ this._cleanLocation(this.props.location) }</td>
         <td>{ this.props.followers }</td>
         <td>{ this._linkToOriginalPost(this.props.link) }</td>
-        <td>
-          <Button
-            bsStyle="success"
-            onClick={ this._handleValidate }>
-            Valider
-          </Button>
-          &nbsp;&nbsp;
-          <Button
-            bsStyle="danger"
-            onClick={ this._handleReject }>
-            Refuser
-          </Button>
-        </td>
+        { btn }
       </tr>
     );
 
@@ -71,6 +79,40 @@ class TweetsRowComponent extends Component {
 
   _handleReject() {
     LivestreamActions.rejectRetweet(this.props.rtId);
+  }
+
+  _handleCancel() {
+    BnfQueueActions.cancelDisplay(this.props.rtId);
+  }
+
+  _actionsLive() {
+    return (
+      <td>
+      <Button
+        onClick={ this._handleValidate }
+        bsStyle="success">
+        Valider
+      </Button>
+      &nbsp;&nbsp;
+      <Button
+        onClick={ this._handleReject }
+        bsStyle="danger">
+        Refuser
+      </Button>
+      </td>
+    );
+  }
+
+  _actionsQueue() {
+    return (
+      <td>
+        <Button
+          bsStyle="danger"
+          onClick={ this._handleCancel }>
+          Annuler
+        </Button>
+      </td>
+    );
   }
 
 }
