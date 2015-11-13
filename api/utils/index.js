e7f6bb98ac5d69ffdd881a64e48ad21fe3b91703
Pygocentrus <pierre.guilhou14@gmail.com>
2015-11-13 12:47:26 +0100

    [feature/api] Shorten URL & DRY code

--- api/utils/index.js
+++ api/utils/index.js
@@ -7,13 +7,18 @@ let Utils = {
   readFilePromisified: function(file) {
     return new Promise((resolve, reject) => {
       fs.readFile(file, (err, data) => {
-        if (!err) {
-          resolve(data);
+        if (!err && data) {
+          resolve(data.toString());
         } else {
           reject(err);
         }
       });
     });
+  },
+
+  isLink: function(link) {
+    let pattern = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+    return pattern.test(str);
   },
 
 };
